8566d590fdab5900b7339e11f2512cd4f282ed63
Scott Corbett <secorbett13@gmail.com>
2017-12-30 12:09:00 -0500

    Got the triangle to display

--- SharpDXJohnFalkTutorial/Game.cs
+++ SharpDXJohnFalkTutorial/Game.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Drawing;
-
+using System.Linq;
 using SharpDX;
-using SharpDX.Windows;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
 using SharpDX.DXGI;
+using SharpDX.Windows;
 
 namespace SharpDXJohnFalkTutorial
 {
@@ -22,6 +24,28 @@ namespace SharpDXJohnFalkTutorial
 
 		private D3D11.RenderTargetView renderTargetView;
 
+		private D3D11.VertexShader vertexShader;
+		private D3D11.PixelShader pixelShader;
+
+		private Vector3[] vertices = new Vector3[]
+		{
+			new Vector3(-0.5f, 0.5f, 0.0f),
+			new Vector3(0.5f, 0.5f, 0.0f),
+			new Vector3(0.0f, -0.5f, 0.0f)
+		};
+
+		private D3D11.Buffer triangleVertexBuffer;
+
+		private D3D11.InputElement[] inputElements = new D3D11.InputElement[]
+		{
+			new D3D11.InputElement("POSITION", 0, Format.R32G32B32_Float, 0)
+		};
+
+		private ShaderSignature inputSignature;
+		private D3D11.InputLayout inputLayout;
+
+		private Viewport viewport;
+
 		/// <summary>
 		/// Constructor for the <see cref="Game"/> class
 		/// </summary>
@@ -33,6 +57,8 @@ namespace SharpDXJohnFalkTutorial
 			renderForm.AllowUserResizing = false;
 
 			InitializeDeviceResources();
+			InitializeShaders();
+			InitializeTriangle();
 		}
 
 		/// <summary>
@@ -48,6 +74,11 @@ namespace SharpDXJohnFalkTutorial
 		/// </summary>
 		public void Dispose()
 		{
+			inputLayout.Dispose();
+			inputSignature.Dispose();
+			triangleVertexBuffer.Dispose();
+			vertexShader.Dispose();
+			pixelShader.Dispose();
 			renderTargetView.Dispose();
 			swapChain.Dispose();
 			d3dDevice.Dispose();
@@ -74,12 +105,18 @@ namespace SharpDXJohnFalkTutorial
 				IsWindowed = true
 			};
 
-			D3D11.Device.CreateWithSwapChain(SharpDX.Direct3D.DriverType.Hardware,
+			// Create device and swap chain
+			D3D11.Device.CreateWithSwapChain(
+				DriverType.Hardware,
 				D3D11.DeviceCreationFlags.None,
 				swapChainDesc,
 				out d3dDevice,
 				out swapChain);
 			d3dDeviceContext = d3dDevice.ImmediateContext;
+
+			// set viewport
+			viewport = new Viewport(0, 0, Width, Height);
+			d3dDeviceContext.Rasterizer.SetViewport(viewport);
 
 			using (D3D11.Texture2D backBuffer = swapChain.GetBackBuffer<D3D11.Texture2D>(0))
 			{
@@ -87,10 +124,56 @@ namespace SharpDXJohnFalkTutorial
 			}
 		}
 
+		private void InitializeShaders()
+		{
+			// Compile the vertex shader code
+			using (var vertexShaderByteCode = ShaderBytecode.CompileFromFile("vertexShader.hlsl", "main", "vs_4_0", ShaderFlags.Debug))
+			{
+				// Read input signature from shader code
+				inputSignature = ShaderSignature.GetInputSignature(vertexShaderByteCode);
+
+				vertexShader = new D3D11.VertexShader(d3dDevice, vertexShaderByteCode);
+			}
+
+			// Compile the pixel shader code
+			using (var pixelShaderByteCode = ShaderBytecode.CompileFromFile("pixelShader.hlsl", "main", "ps_4_0", ShaderFlags.Debug))
+			{
+				pixelShader = new D3D11.PixelShader(d3dDevice, pixelShaderByteCode);
+			}
+
+			// set as the current vertex and pixel shaders
+			d3dDeviceContext.VertexShader.Set(vertexShader);
+			d3dDeviceContext.PixelShader.Set(pixelShader);
+
+			d3dDeviceContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+
+			// Create the input layout from the inputsignature and the input elements
+			inputLayout = new D3D11.InputLayout(d3dDevice, inputSignature, inputElements);
+
+			// Set the input layout to use
+			d3dDeviceContext.InputAssembler.InputLayout = inputLayout;
+		}
+
+		private void InitializeTriangle()
+		{
+			triangleVertexBuffer = D3D11.Buffer.Create<Vector3>(d3dDevice, D3D11.BindFlags.VertexBuffer, vertices);
+		}
+
 		private void Draw()
 		{
+			// Set the back buffere as teh current render target view
 			d3dDeviceContext.OutputMerger.SetRenderTargets(renderTargetView);
+
+			// Clear the screen
 			d3dDeviceContext.ClearRenderTargetView(renderTargetView, new SharpDX.Color(32, 103, 178));
+
+			// Set vertex buffere
+			d3dDeviceContext.InputAssembler.SetVertexBuffers(0, new D3D11.VertexBufferBinding(triangleVertexBuffer, Utilities.SizeOf<Vector3>(), 0));
+
+			// Draw the triangle
+			d3dDeviceContext.Draw(vertices.Count(), 0);
+
+			// Swap the front and back buffer
 			swapChain.Present(1, PresentFlags.None);
 		}
 	}
