69a1590378ae3cd1a436a06a0dbbb901344c9525
Scott Corbett <secorbett13@gmail.com>
2018-01-06 15:29:33 -0500

    Updated the copyright and cleaned up some of the formatting.

--- SharpDXStarter/AppConfiguration.cs
+++ SharpDXStarter/AppConfiguration.cs
@@ -1,4 +1,33 @@
-﻿namespace SharpDXStarter
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppConfiguration.cs" company="N/A">
+// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//   Copyright (c) 2018 Scott E. Corbett.
+// </copyright>
+// <summary>
+//   The app configuration.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SharpDXStarter
 {
 	/// <summary>
 	/// The app configuration.
@@ -6,7 +35,7 @@
 	public class AppConfiguration
 	{
 		/// <summary>
-		/// Initializes a new instance of the <see cref="AppConfiguration"/> class.
+		/// Initializes a new instance of the <see cref="T:SharpDXStarter.AppConfiguration" /> class.
 		/// </summary>
 		public AppConfiguration()
 			: this("A demo app. Don't forget the title", 800, 600, false)
@@ -14,18 +43,18 @@
 		}
 		
 		/// <summary>
-				/// Initializes a new instance of the <see cref="AppConfiguration"/> class.
-				/// </summary>
-				/// <param name="title">
-				/// The title of the window created with the configuration.
-				/// </param>
+		/// Initializes a new instance of the <see cref="T:SharpDXStarter.AppConfiguration" /> class.
+		/// </summary>
+		/// <param name="title">
+		/// The title of the window created with the configuration.
+		/// </param>
 		public AppConfiguration(string title)
 			: this(title, 800, 600, false)
 		{
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="AppConfiguration"/> class.
+		/// Initializes a new instance of the <see cref="T:SharpDXStarter.AppConfiguration" /> class.
 		/// </summary>
 		/// <param name="title">
 		/// The title of the window created with the configuration.
